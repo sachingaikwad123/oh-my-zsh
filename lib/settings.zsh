902932324016206e714a3d1cb7d8dcc663a76339
Sachin Gaikwad <sachingaikwad123@gmail.com>
2012-09-05 21:47:16 -0700

    Add new settings in lib

---
+++ lib/settings.zsh
@@ -0,0 +1,13 @@
+
+# General Settings: Sachin Gaikwad
+
+# exports
+export EDITOR=vim
+export TMPDIR=~/tmp/
+export PATH=$PATH:$HOME/bin
+
+# So that ctrl-q and ctrl-s dont mess up with my terminal and freeze it
+# for putty
+stty start ^_
+stty stop ^_
+
