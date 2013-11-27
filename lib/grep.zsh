3f7d9c49d8a0c76a559e3f7604e96d68f135f6cb
Sachin Gaikwad <sachingaikwad123@gmail.com>
2013-11-27 01:19:33 -0800

    Disable colored grep
    - It creates problems in my shell scripts used for testing

--- lib/grep.zsh
+++ lib/grep.zsh
@@ -1,12 +1,2 @@
-if [[ "$DISABLE_COLOR" != 'true' ]]; then
-  if [[ -z "$GREP_OPTIONS" ]]; then
-    export GREP_OPTIONS='--color=always'
-  fi
-  if [[ -z "$GREP_COLOR" ]]; then
-    export GREP_COLOR="1;32"
-  fi
-else
-  export GREP_OPTIONS='--color=none'
-  export GREP_COLOR=''
-fi
-
+export GREP_OPTIONS='--color=none'
+export GREP_COLOR=''
