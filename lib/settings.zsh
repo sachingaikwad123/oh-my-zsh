1ceb094f7939e45387c2a08e3aa5decddbfe6735
Sachin Gaikwad <sachingaikwad123@gmail.com>
2013-11-27 01:25:52 -0800

    Set SVN_EDITOR to my own svn editor

--- lib/settings.zsh
+++ lib/settings.zsh
@@ -5,6 +5,7 @@
 export EDITOR=vim
 export TMPDIR=~/tmp/
 export PATH=$PATH:$HOME/bin
+export SVN_EDITOR=$HOME/bin/svn-editor
 
 # So that ctrl-q and ctrl-s dont mess up with my terminal and freeze it
 # for putty
